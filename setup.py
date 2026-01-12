fc096b3bc25158394f459014c70a7963fdca8f95
Puranjay Saha <puranjay7100@gmail.com>
2026-01-12 08:42:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Puranjay Saha',
+    author_email='puranjay7100@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
